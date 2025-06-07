c1418873d40eeeafa47df8bfded328e856e1ed66
surajstudent9112@gmail.com <surajstudent9122@gmail.com>
2025-06-07 11:41:29 +0530

    language options

--- src/components/LanguageOptions.jsx
+++ src/components/LanguageOptions.jsx
@@ -3,36 +3,19 @@ import React from "react";
 const LanguageOptions = () => {
   return (
     <div className="mt-8 text-sm text-gray-600">
-      Google offered in:{" "}
-      <a href="#" className="text-blue-600 hover:underline">
-        हिन्दी
-      </a>{" "}
-      <a href="#" className="text-blue-600 hover:underline">
-        বাংলা
-      </a>{" "}
-      <a href="#" className="text-blue-600 hover:underline">
-        తెలుగు
-      </a>{" "}
-      <a href="#" className="text-blue-600 hover:underline">
-        मराठी
-      </a>{" "}
-      <a href="#" className="text-blue-600 hover:underline">
-        தமிழ்
-      </a>{" "}
-      <a href="#" className="text-blue-600 hover:underline">
-        ગુજરાતી
-      </a>{" "}
-      <a href="#" className="text-blue-600 hover:underline">
-        ಕನ್ನಡ
-      </a>{" "}
-      <a href="#" className="text-blue-600 hover:underline">
-        മലയാളം
-      </a>{" "}
-      <a href="#" className="text-blue-600 hover:underline">
-        ਪੰਜਾਬੀ
-      </a>
+      Google offered in:{' '}
+      <a href="#" className="text-blue-600 hover:underline">हिन्दी</a>{' '}
+      <a href="#" className="text-blue-600 hover:underline">বাংলা</a>{' '}
+      <a href="#" className="text-blue-600 hover:underline">తెలుగు</a>{' '}
+      <a href="#" className="text-blue-600 hover:underline">मराठी</a>{' '}
+      <a href="#" className="text-blue-600 hover:underline">தமிழ்</a>{' '}
+      <a href="#" className="text-blue-600 hover:underline">ગુજરાતી</a>{' '}
+      <a href="#" className="text-blue-600 hover:underline">ಕನ್ನಡ</a>{' '}
+      <a href="#" className="text-blue-600 hover:underline">മലയാളം</a>{' '}
+      <a href="#" className="text-blue-600 hover:underline">ਪੰਜਾਬੀ</a>
     </div>
   );
 };
+
 
 export default LanguageOptions;
